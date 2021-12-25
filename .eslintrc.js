a716f7263e7821b13e7a626932143e3208e19a70
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2021-12-25 14:51:26 +0900

    eslintの設定した

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "import"
+    ],
+    "rules": {
+        "no-unused-vars": "warn",
+        "import/newline-after-import": "error"
+    }
+};
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+import fs from "fs";
+
 var a = 1
 
 var b = () => {
     return 2
 }
 
+console.log(fs)
 console.log(a)
 console.log(b())
-
-
