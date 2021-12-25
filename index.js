a1d6d35182e26469b3aa4f15003f0dfd9c3f79cc
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2021-12-25 15:53:29 +0900

    typescript入れた

---
+++ index.js
@@ -0,0 +1,6 @@
+var a = 1;
+var b = function () {
+    return 2;
+};
+console.log(a);
+console.log(b());
--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
-import fs from "fs";
-
-var a = 1
+var a: number = 1
 
 var b = () => {
     return 2
 }
 
-console.log(fs)
 console.log(a)
 console.log(b())
